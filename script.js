1d6ba69dae93ef67c8711cd77b8d6eeb1f0a4479
Juliana dos Santos Freitas <jujugemeasf@gmail.com>
2021-03-09 20:12:45 -0300

    cobrinha se movendo

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box)
+    context.fillRect( 0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
@@ -20,6 +20,15 @@ function criarCobrinha(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+document.addEventListener('keydown', update);
+
+function update (event) {
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo(){
