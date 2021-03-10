5d32a7919280d9f01498f21d6f48f13d53a01429
Juliana dos Santos Freitas <jujugemeasf@gmail.com>
2021-03-09 21:35:23 -0300

    Colocando dificudades
    A cobrinha não pode tocar nela mesma.

--- script.js
+++ script.js
@@ -42,10 +42,18 @@ function update (event) {
 }
 
 function iniciarJogo(){
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -59,7 +67,13 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
